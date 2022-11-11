4b5bd2c22460ed776bdbcb244373ad38f579703a
ford-narongrit <narongrit.th@ku.th>
2022-11-11 15:30:29 +0700

    edit vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Devcamp-vue-101/",
+  base: "/animalpower/",
   plugins: [vue()],
   resolve: {
     alias: {
